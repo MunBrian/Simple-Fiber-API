8dac035b01dd9595ac63a89107f04b1a0ce5b0bc
Brian Mungai <bryanmungai254@gmail.com>
2023-01-18 12:37:28 +0300

    added get all books and get book by id using id from url param

--- main.go
+++ main.go
@@ -4,11 +4,42 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+// make array of struct book
+var books = []book{
+	{ID: "1", Title: "In serach of Lost time", Author: "Mike Wills", Quantity: 2},
+	{ID: "2", Title: "How times have changed", Author: "John Doe", Quantity: 10},
+	{ID: "3", Title: "Interstellar", Author: "Dr.Hammerman", Quantity: 10},
+	{ID: "4", Title: "African Woman", Author: "Lucy Wairimu", Quantity: 14},
+}
+
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!!")
+	})
+
+	app.Get("/books", func(c *fiber.Ctx) error {
+		return c.JSON(books)
+	})
+
+	app.Get("/books/:id", func(c *fiber.Ctx) error {
+		bookId := c.Params("id")
+
+		for _, book := range books {
+			if book.ID == bookId {
+				return c.JSON(book)
+			}
+		}
+
+		return c.SendString("No book found")
 	})
 
 	app.Listen(":8000")
